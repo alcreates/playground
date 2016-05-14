a451dde99e6c149eb8cc64f759a7806d21c34d95
Alvaro Martinez <alvaromartinez@nbp-81-212.nbp.ruw.rutgers.edu>
2016-05-14 12:06:26 -0400

    connection

--- app/config/connection.js
+++ app/config/connection.js
@@ -15,10 +15,10 @@ var source = {
 
     jawsDB: {
         port: 3306,
-        host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'cm0zdmk2ez4igof5',
-        password: "yuhl98jsef0k7ul4",
-        database: "" 
+        host: 'izm96dhhnwr2ieg0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'tmbe2cclke22jl0m',
+        password: "ivwn05n5renz13kd",
+        database: "prz7uvdzcprfg4jq" 
     }
 }
 
